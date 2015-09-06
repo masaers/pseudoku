ff3efc4c81536ea033c7784e5af3c028863d2982
masaers <masaers@gmail.com>
2015-09-06 14:30:50 +0800

    Introduced a solver class
    Turned the “smarter” strategy into a solver class. This allowed for the
    solver to add the provided numbers straight to the agenda and skip the
    brut-forcing step.
    
    Scrapped the strategy comparison of the main class.

--- pseudoku.cpp
+++ pseudoku.cpp
@@ -162,7 +162,165 @@ public:
 protected:
   cell_type board_m[N][N];
   int unknown_m;
-};
+}; // pseudoku
+
+
+class pseudoku_solver {
+public:
+  typedef std::pair<int, int> coord_type;
+  typedef std::deque<coord_type> agenda_type;
+  agenda_type& agenda() { return agenda_m; }
+  void operator()(pseudoku& s) {
+    using namespace std;
+    auto solved_it = back_inserter(agenda_m);
+    // brute_force_obvious(s, solved_it);
+    while (! empty()) {
+      const int i = top_row();
+      const int j = top_col();
+      pop();
+      s.propagate_solution(i, j, solved_it);
+    }
+    while (true) {
+      for (int row = 0; row < N; ++row) {
+        // Analyze row
+        cell_type backward[N];
+        cell_type field_out[M];
+        cell_type field_in[M];
+        unordered_map<cell_type, unordered_set<int> > uniq_sets;
+        for (int col = 0; col < N; ++col) {
+          if (col != 0) {
+            backward[N-(col+1)] = backward[N-col] | s(row, N-col);
+          }
+          const int f = s.field(row, col) % M;
+          const cell_type& cell = s(row, col);
+          field_in[f] |= cell;
+          for (int foff = 1; foff < M; ++foff) {
+            field_out[(f + foff) % M] |= cell;
+          }
+          if(cell.count() > 1) {
+            uniq_sets[cell].insert(col);
+          }
+        }
+        // Find unique sets and eliminate their members from other cells
+        // in the same row
+        for (auto it = begin(uniq_sets); it != end(uniq_sets); ++it) {
+          if (it->first.count() == it->second.size()) {
+            for (auto col = 0; col < N; ++col) {
+              if (it->second.find(col) == it->second.end()) {
+                s.apply_mask(row, col, ~it->first, solved_it);
+              }
+            }
+          }
+        }
+        // Find cells that have row-unique numbers and commit to them
+        cell_type forward;
+        for (int col = 0; col < N; ++col) {
+          const cell_type mask = ~(backward[col] | forward);
+          s.try_mask(row, col, mask, solved_it);
+          forward |= s(row, col);
+        }
+        // Find rows in fields that have row-unique numbers and
+        // eliminate the numbers from rest of field.
+        for (int fc = 0; fc < M; ++fc) {
+          const cell_type mask = field_in[fc] & ~field_out[fc];
+          if (mask.any()) {
+            const int f = s.field(row, fc * M);
+            for (int c = 0; c < N; ++c) {
+              if (s.frow(f, c) != row) {
+                s.apply_mask(s.frow(f, c), s.fcol(f, c), ~mask, solved_it);
+              }
+            }
+          }
+        }
+      } // rows
+      for (int col = 0; col < N; ++col) {
+        // Analyze column
+        cell_type backward[N];
+        cell_type field_out[M];
+        cell_type field_in[M];
+        unordered_map<cell_type, unordered_set<int> > uniq_sets;
+        backward[N-1] = cell_type();
+        for (int row = 0; row < N; ++row) {
+          if (row != 0) {
+            backward[N-(row+1)] = backward[N-row] | s(N-row, col);
+          }
+          const int f = s.field(row, col) / M;
+          const cell_type& cell = s(row, col);
+          field_in[f] |= cell;
+          for (int foff = 1; foff < M; ++foff) {
+            field_out[(f + foff) % M] |= cell;
+          }
+          if(cell.count() > 1) {
+            uniq_sets[cell].insert(row);
+          }
+        }
+        // Find unique sets and eliminate their members from other cells
+        // in the same column
+        for (auto it = begin(uniq_sets); it != end(uniq_sets); ++it) {
+          if (it->first.count() == it->second.size()) {
+            for (auto row = 0; row < N; ++row) {
+              if (it->second.find(row) == it->second.end()) {
+                s.apply_mask(row, col, ~it->first, solved_it);
+              }
+            }
+          }
+        }
+        // Find cells that have column-unique numbers and commit to them
+        cell_type forward;
+        for (int row = 0; row < N; ++row) {
+          s.try_mask(row, col, ~backward[row] & ~forward, solved_it);
+          forward |= s(row, col);
+        }
+        // Find columns in fields that have column-unique numbers and
+        // eliminate the numbers from rest of field.
+        for (int fc = 0; fc < M; ++fc) {
+          const cell_type mask = field_in[fc] & ~field_out[fc];
+          if (mask.any()) {
+            const int f = s.field(fc * M, col);
+            for (int c = 0; c < N; ++c) {
+              if (s.fcol(f, c) != col) {
+                s.apply_mask(s.frow(f, c), s.fcol(f, c), ~mask, solved_it);
+              }
+            }
+          }
+        }
+      } // columns
+      for (int field = 0; field < N; ++field) {
+        // Analyze field
+        cell_type backward[N];
+        backward[N-1] = cell_type();
+        for (int f = N-2; f >= 0; --f) {
+          backward[f] = backward[f+1] | s(s.frow(field, f+1), s.fcol(field, f+1));
+        }
+        // Find cells that have field-unique numbers and commit to them
+        cell_type forward;
+        for (int f = 0; f < N; ++f) {
+          s.try_mask(s.frow(field, f), s.fcol(field, f),
+                     ~backward[f] & ~forward,
+                     solved_it);
+          forward |= s(s.frow(field, f), s.fcol(field, f));
+        }
+      } // fields
+      if (empty()) {
+        // Failed to solve any cells, give up.
+        break;
+      }
+      while (! empty()) {
+        const int i = top_row();
+        const int j = top_col();
+        pop();
+        s.propagate_solution(i, j, solved_it);
+      }
+    }
+  }
+  const int top_row() const { return agenda_m.front().first; }
+  const int top_col() const { return agenda_m.front().second; }
+  void pop() { agenda_m.pop_front(); }
+  bool empty() const { return agenda_m.empty(); }
+protected:
+  agenda_type agenda_m;
+}; // pseudoku_solver
+
 
 template<typename solved_it_T>
 int brute_force_obvious(pseudoku& s, solved_it_T&& solved_it) {
@@ -208,186 +366,28 @@ int silly_fast(pseudoku& s) {
   return result;
 }
 
-int smarter(pseudoku& s) {
-  using namespace std;
-  cout << "strategy: smarter" << endl;
-  int result = 0;
-  deque<pair<int, int>> todo;
-
-  result += brute_force_obvious(s, back_inserter(todo));
-  while (! todo.empty()) {
-    const int i = todo.front().first;
-    const int j = todo.front().second;
-    todo.pop_front();
-    result += s.propagate_solution(i, j, back_inserter(todo));
-  }
-  while (true) {
-    auto solved_it = back_inserter(todo);
-    for (int row = 0; row < N; ++row) {
-      // Analyze row
-      cell_type backward[N];
-      cell_type field_out[M];
-      cell_type field_in[M];
-      unordered_map<cell_type, unordered_set<int> > uniq_sets;
-      for (int col = 0; col < N; ++col) {
-        if (col != 0) {
-          backward[N-(col+1)] = backward[N-col] | s(row, N-col);
-          ++result;
-        }
-        const int f = s.field(row, col) % M;
-        const cell_type& cell = s(row, col);
-        ++result;
-        field_in[f] |= cell;
-        for (int foff = 1; foff < M; ++foff) {
-          field_out[(f + foff) % M] |= cell;
-        }
-        if(cell.count() > 1) {
-          uniq_sets[cell].insert(col);
-        }
-      }
-      // Find unique sets and eliminate their members from other cells
-      // in the same row
-      for (auto it = begin(uniq_sets); it != end(uniq_sets); ++it) {
-        if (it->first.count() == it->second.size()) {
-          for (auto col = 0; col < N; ++col) {
-            if (it->second.find(col) == it->second.end()) {
-              s.apply_mask(row, col, ~it->first, solved_it);
-              ++result;
-            }
-          }
-        }
-      }
-      // Find cells that have row-unique numbers and commit to them
-      cell_type forward;
-      for (int col = 0; col < N; ++col) {
-        const cell_type mask = ~(backward[col] | forward);
-        s.try_mask(row, col, mask, solved_it);
-        forward |= s(row, col);
-        result += 2;
-      }
-      // Find rows in fields that have row-unique numbers and
-      // eliminate the numbers from rest of field.
-      for (int fc = 0; fc < M; ++fc) {
-        const cell_type mask = field_in[fc] & ~field_out[fc];
-        if (mask.any()) {
-          const int f = s.field(row, fc * M);
-          for (int c = 0; c < N; ++c) {
-            if (s.frow(f, c) != row) {
-              s.apply_mask(s.frow(f, c), s.fcol(f, c), ~mask, solved_it);
-            }
-          }
-        }
-      }
-    } // rows
-    for (int col = 0; col < N; ++col) {
-      // Analyze column
-      cell_type backward[N];
-      cell_type field_out[M];
-      cell_type field_in[M];
-      unordered_map<cell_type, unordered_set<int> > uniq_sets;
-      backward[N-1] = cell_type();
-      for (int row = 0; row < N; ++row) {
-        if (row != 0) {
-          backward[N-(row+1)] = backward[N-row] | s(N-row, col);
-          ++result;
-        }
-        const int f = s.field(row, col) / M;
-        const cell_type& cell = s(row, col);
-        ++result;
-        field_in[f] |= cell;
-        for (int foff = 1; foff < M; ++foff) {
-          field_out[(f + foff) % M] |= cell;
-        }
-        if(cell.count() > 1) {
-          uniq_sets[cell].insert(row);
-        }
-      }
-      // Find unique sets and eliminate their members from other cells
-      // in the same column
-      for (auto it = begin(uniq_sets); it != end(uniq_sets); ++it) {
-        if (it->first.count() == it->second.size()) {
-          for (auto row = 0; row < N; ++row) {
-            if (it->second.find(row) == it->second.end()) {
-              s.apply_mask(row, col, ~it->first, solved_it);
-              ++result;
-            }
-          }
-        }
-      }
-      // Find cells that have column-unique numbers and commit to them
-      cell_type forward;
-      for (int row = 0; row < N; ++row) {
-        s.try_mask(row, col, ~backward[row] & ~forward, solved_it);
-        forward |= s(row, col);
-      }
-      result += N;
-      // Find columns in fields that have column-unique numbers and
-      // eliminate the numbers from rest of field.
-      for (int fc = 0; fc < M; ++fc) {
-        const cell_type mask = field_in[fc] & ~field_out[fc];
-        if (mask.any()) {
-          const int f = s.field(fc * M, col);
-          for (int c = 0; c < N; ++c) {
-            if (s.fcol(f, c) != col) {
-              s.apply_mask(s.frow(f, c), s.fcol(f, c), ~mask, solved_it);
-            }
-          }
-        }
-      }
-    } // columns
-    for (int field = 0; field < N; ++field) {
-      // Analyze field
-      cell_type backward[N];
-      backward[N-1] = cell_type();
-      for (int f = N-2; f >= 0; --f) {
-        backward[f] = backward[f+1] | s(s.frow(field, f+1), s.fcol(field, f+1));
-      }
-      result += N;
-      // Find cells that have field-unique numbers and commit to them
-      cell_type forward;
-      for (int f = 0; f < N; ++f) {
-        s.try_mask(s.frow(field, f), s.fcol(field, f),
-                   ~backward[f] & ~forward,
-                   solved_it);
-        forward |= s(s.frow(field, f), s.fcol(field, f));
-      }
-    } // fields
-    if (todo.empty()) {
-      // Failed to solve any cells, give up.
-      break;
-    }
-    while (! todo.empty()) {
-      const int i = todo.front().first;
-      const int j = todo.front().second;
-      todo.pop_front();
-      result += s.propagate_solution(i, j, back_inserter(todo));
-    }
-  }
-  return result;
-}
 
 int main(const int argc, const char** argv) {
   using namespace std;
-  
-  pseudoku start;
-  start.read(cin, null_iterator());
-  start.print(cout);
+
+  pseudoku board;
+  pseudoku_solver solve;
+  board.read(cin, back_inserter(solve.agenda()));
+  if (! board.valid()) {
+    cerr << "Provided board not valid." << endl;
+    return EXIT_FAILURE;
+  }
+  board.print(cout);
   cout << endl;
-  if (! start.valid()) {
-    cout << "Initial board is invalid!" << endl;
-    return 1;
+  
+  solve(board);
+  
+  if (! board.solved()) {
+    cout << "Failed to find a solution :-)" << endl;
   }
-  vector<function<int(pseudoku&)>> strategies({silly, silly_fast, smarter});
-  for (const auto strategy : strategies) {
-    pseudoku s(start);
-    int inspected = strategy(s);
-    cout << "inspected " << inspected << " cells." << endl;
-    if (! s.solved()) {
-      cout << "Filed to find a solution :-(" << endl;
-    }
-    cout << "Board is " << (s.valid() ? "" : "in") << "valid." << endl;
-    s.print(cout);
-    cout << endl;
-  }
-  return 0;
+  cout << "Board is " << (board.valid() ? "" : "in") << "valid." << endl;
+  board.print(cout);
+  cout << endl;
+  
+  return EXIT_SUCCESS;
 }
